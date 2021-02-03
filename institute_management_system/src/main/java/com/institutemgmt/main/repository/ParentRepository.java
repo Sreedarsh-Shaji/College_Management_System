a4b05798614d1b78cc401809d9d3685c2344062a
Sreedarsh Dell PC <SreedarshShaji@outlook.com>
2021-02-04 01:10:48 +0530

    React app initial setup

--- institute_management_system/src/main/java/com/institutemgmt/main/repository/ParentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.institutemgmt.main.repository;
-
-import com.institutemgmt.main.entites.Parent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ParentRepository extends JpaRepository<Parent, Integer> {
-}
--- institute_management_system/src/main/java/com/institutemgmt/main/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.institutemgmt.main.repository;
-
-import com.institutemgmt.main.entites.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-}
